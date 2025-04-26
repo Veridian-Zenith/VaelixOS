17983da6955195b19fb0da86e2b6f1f719ad5eb6
Dae Euhwa <daedaevibin@naver.com>
2025-04-26 17:10:22 -0500

    cleanup..

---
+++ target/debug/deps/test_main-c5e18f1e7ee212b4.d
@@ -0,0 +1,5 @@
+/home/dae/Veridian-Zenith/VaelixOS/target/debug/deps/libtest_main-c5e18f1e7ee212b4.rmeta: tests/test_main.rs
+
+/home/dae/Veridian-Zenith/VaelixOS/target/debug/deps/test_main-c5e18f1e7ee212b4.d: tests/test_main.rs
+
+tests/test_main.rs:
---
+++ target/debug/examples/example_main-d715174dad2125b2.d
@@ -0,0 +1,5 @@
+/home/dae/Veridian-Zenith/VaelixOS/target/debug/examples/libexample_main-d715174dad2125b2.rmeta: examples/example_main.rs
+
+/home/dae/Veridian-Zenith/VaelixOS/target/debug/examples/example_main-d715174dad2125b2.d: examples/example_main.rs
+
+examples/example_main.rs:
